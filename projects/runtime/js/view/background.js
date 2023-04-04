df219c883338979f08ce28bd30e20bde5fc71e88
Nicholas Veazey <njv0199230@lpssonline.com>
2023-04-04 13:53:56 +0000

    add runtime project

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -26,7 +26,10 @@ var background = function (window) {
         var background;
         
         // ANIMATION VARIABLES HERE:
-        
+        tree = draw.bitmap("img/tree.png");
+        tree.x = 0;
+        tree.y = 0;
+        background.addChild(tree);
      
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
@@ -36,16 +39,22 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,groundY,'crimson');
+            var backgroundFill = draw.rect(canvasWidth,groundY,"crimson");
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
             var moon = draw.bitmap("img/moon.png");
-            moon.x = 1200;
-            moon.y = 00;
-            moon.scaleX = 2.0;
-            moon.scaleY = 2.0;
-            background.addChild(moon);  
+            moon.x = 1700;
+            moon.y = 50;
+            moon.scaleX = .750;
+            moon.scaleY = .750;
+            background.addChild(moon);
+            for (var i = 0 ; i<200; i++){
+                var circle = draw.circle(10, "white", "LightGray", 2);
+                circle.x = canvasWidth * Math.random();
+                circle.y = groundY * Math.random();
+                background.addChild(circle);
+            }
             
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             
@@ -87,11 +96,11 @@ var background = function (window) {
         render();
         return background;
     };
-};
+
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
-    (typeof process.versions.node !== 'undefined')) {
+    (typeof process.versions.node !== 'undefined')) 
     // here, export any references you need for tests //
-    module.exports = background;
+    module.exports = background
 }
